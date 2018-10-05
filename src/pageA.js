2178c6456c45b55822da990273b340cacf89cd56
luoquanquan <luoquanquan@baidu.com>
2018-10-05 10:39:47 +0800

    通过 require.ensure 实现代码分包

--- src/pageA.js
+++ src/pageA.js
@@ -1,6 +1,13 @@
 import SubPageA from './subPageA'
 import SubPageB from './subPageB'
 
-import * as _ from 'lodash'
+// import * as _ from 'lodash'
+
+// 这一句只是加载 lodash
+require.ensure(['lodash'],function() {
+    // 使用这一个 require 才会执行加载的代码
+    var _ = require('lodash')
+    console.log(_)
+}, 'vender')
 
 export default 'PageA'
