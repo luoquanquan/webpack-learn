55daf447637a07b73b37a15a61b24a5ed52b6302
luoquanquan <luoquanquan@baidu.com>
2018-10-06 11:17:02 +0800

    css-loader -> style-loader 简单css 配置

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,7 @@ const path = require('path')
 
 module.exports = {
     entry: {
-        pageA: './src/pageA.js',
-        pageB: './src/pageB.js',
-        vender: ['lodash']
+        app: './src/app.js',
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -13,15 +11,19 @@ module.exports = {
         filename: '[name].bundle.js',
         chunkFilename: '[name].chunk.js'
     },
-    plugins: [
-        new webpack.optimize.CommonsChunkPlugin({
-            async: 'async-common',
-            children: true,
-            minChunks: 2
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            names: ['vender', 'manifest'],
-            minChunks: Infinity
-        })
-    ]
+    module: {
+        rules: [
+            {
+                test: /.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    }
+                ]
+            }
+        ]
+    }
 }
