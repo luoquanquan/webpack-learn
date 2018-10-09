31d316d2210a10a71a6fc946f4de97df1b503c48
luoquanquan <luoquanquan@baidu.com>
2018-10-09 22:58:22 +0800

    没有使用树摇前, 所有的方法都被导入了

--- src/app.js
+++ src/app.js
@@ -1,5 +1,2 @@
-import base from './css/base.less'
-import common from './css/common.less'
-
-var app = document.getElementById('app')
-app.innerHTML = '<div class="'+ base.box +'"></div>'
+import { a } from './common/util'
+a()
